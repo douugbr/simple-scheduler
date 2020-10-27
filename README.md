e2729f099596c3e4c672eae0b1ce8882f3ba6693
DOUGLAS EDUARDO REIS SILVA <douugbr@gmail.com>
2020-10-27 08:25:36 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,12 +1,1 @@
-# qr-rick-roll
-Generates random QR codes that all link up to Rick Astley's Never Gonna Give You Up.
 
-Made with flask.
-
-# Usage
-
-``cd qr-rick-roll``
-
-``flask run``
-
-Then it should start a local web server that generates different qrcodes everytime you reload it.
--- app.py
+++ app.py
@@ -1,20 +1,7 @@
-from flask import Flask, render_template, send_file
-import qrcode
-from random import randint
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def generate():
-    additional_char = ''
-    for i in range(randint(1, 50)):
-        for j in range(randint(1, 3)):
-            additional_char += '?'
-        for j in range(randint(1, 3)):
-            additional_char += '='
-
-    text = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' + additional_char
-    qr = img = qrcode.make(text)
-    qr.save('qr.png')
-    response = send_file('qr.png', mimetype='image/gif')
-    return response
+def login_page():
+    return 'Hello World!'
