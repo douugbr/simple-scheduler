42b58443174d0757d264b0fe67f92d997682597e
DOUGLAS EDUARDO REIS SILVA <douugbr@gmail.com>
2020-10-27 22:09:42 -0300

    Added login system

--- app.py
+++ app.py
@@ -1,7 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, g, redirect, url_for
+from flask_oidc import OpenIDConnect
+from okta import UsersClient
 
 app = Flask(__name__)
+app.config["OIDC_CLIENT_SECRETS"] = "client_secrets.json"
+app.config["OIDC_COOKIE_SECURE"] = False
+app.config["OIDC_CALLBACK_ROUTE"] = "/oidc/callback"
+app.config["OIDC_SCOPES"] = ["openid", "email", "profile"]
+app.config["SECRET_KEY"] = "K2dHBgurIAlwPmdbjXFgxQ"
+oidc = OpenIDConnect(app)
+okta_client = UsersClient("https://dev-1566490.okta.com", '00yg_xIMWwM-VzV63Mh8fX03ZwZvkEaF14Q1HJ2LeW')
 
-@app.route('/')
-def login_page():
-    return 'Hello World!'
+@app.before_request
+def before_request():
+    if oidc.user_loggedin:
+        g.user = okta_client.get_user(oidc.user_getfield("sub"))
+    else:
+        g.user = None
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/scheduler")
+@oidc.require_login
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route("/login")
+@oidc.require_login
+def login():
+    return redirect(url_for(".dashboard"))
+
+
+@app.route("/logout")
+def logout():
+    oidc.logout()
+    return redirect(url_for(".index"))
+
